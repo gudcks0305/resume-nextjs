f75f724a84df827f5bd143782e58fd4e3e0a4407
유형찬 <yuhyeongchan@yuhyeongchan-ui-MacBookPro.local>
2026-05-12 21:56:40 +0900

    Remove selected resume projects

--- payload/project.ts
+++ payload/project.ts
@@ -1,14 +1,11 @@
 import { IProject } from '../component/project/IProject';
 import { acca } from './project_list/acca';
-import { yataProject } from './project_list/yataProject';
-import { gdscBlog } from './project_list/gdscblog';
-import { ociKubernetesProject } from './project_list/ociKubernetes';
 import { aimsWeb } from './project_list/aimsWeb';
 import { pageone } from './project_list/pageone';
 import { mailbox } from './project_list/mailbox';
 
 const project: IProject.Payload = {
   disable: false,
-  list: [acca, pageone, aimsWeb, ociKubernetesProject, mailbox, yataProject, gdscBlog],
+  list: [acca, pageone, aimsWeb, mailbox],
 };
 export default project;
