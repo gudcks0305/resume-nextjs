14632adb6e7bc566594fd815b96872ba867395e3
yoo hyeong chan <84506708+gudcks0305@users.noreply.github.com>
2026-08-26 14:07:23 +0900

    chore: remove phone number from public resume

--- payload/profile.ts
+++ payload/profile.ts
@@ -1,4 +1,4 @@
-import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import image from '../asset/preview.jpg';
 import { IProfile } from '../component/profile/IProfile';
@@ -6,7 +6,6 @@ import { IProfile } from '../component/profile/IProfile';
 const profile: IProfile.Payload = {
   disable: false,
 
-  // image: 'https://resume.yowu.dev/static/image/profile_2019.png',
   image: image.src,
   name: {
     title: '유형찬',
@@ -19,33 +18,13 @@ const profile: IProfile.Payload = {
       icon: faEnvelope,
     },
     {
-      title: '010-9132-7434',
-      icon: faPhone,
-      badge: true,
-    },
-    {
       title: 'https://github.com/gudcks0305',
       link: 'https://github.com/gudcks0305',
       icon: faGithub,
     },
-    /*    {
-      link: 'https://velog.io/@gudcks0305',
-      icon: faBlog,
-    }, */
-    // {
-    //   title: '희망 연봉 : 38,000,000',
-    //   icon: faMoneyBill,
-    // },
-    // {
-    //   title: 'YouTube',
-    //   link: 'https://www.youtube.com/channel/UC3SyT4_WLHzN7JmHQwKQZww',
-    //   // icon: faRss,
-    //   icon: faYoutube,
-    // },
   ],
   notice: {
     title: '☀️ 더 나은 실패를 하라 - 사뮈엘 베케트',
-    // icon: faBell,
   },
 };
 
